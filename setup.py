72038914bf3df189550849d590ea479197c71d96
Joshua Ladd <jladd.mlnx@gmail.com>
2021-06-07 18:14:26 -0400

    Fix Copyright Date. (#3)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) Mellanox Technologies Ltd. 2020-2021.  ALL RIGHTS RESERVED.
+# Copyright (C) Mellanox Technologies Ltd. 2001-2021.
 #
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
