867e44533169b63d6ecad604a4f875acf2b80fc4
Srinivas Sridharan <ssrinivas@fb.com>
2021-06-07 22:31:22 -0700

    Replace allgather_base by _allgather_base

--- include/torch_ucc.hpp
+++ include/torch_ucc.hpp
@@ -185,7 +185,7 @@ class ProcessGroupUCC : public ProcessGroup {
       std::vector<at::Tensor>& inputTensors,
       const AllgatherOptions& opts = AllgatherOptions()) override;
 
-  c10::intrusive_ptr<ProcessGroup::Work> allgather_base(
+  c10::intrusive_ptr<ProcessGroup::Work> _allgather_base(
       at::Tensor& outputBuffer,
       at::Tensor& inputBuffer,
       const AllgatherOptions& opts = AllgatherOptions()) override;
