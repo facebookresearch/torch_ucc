7bc24c77c97ce5ab33a5ee7dea85bffc46823e1a
Ching-Hsiang Chu <chchu@fb.com>
2021-11-11 09:46:32 -0800

    fix logger to show real line number (#50)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/torch_ucc/pull/50
    
    - Use C macro instead of inline function to allow glog printing out real line number
      - `logger->logError` -> `TORCH_UCC_LOG_ERROR`
      - `logger->logInfo` -> `TORCH_UCC_LOG_INFO`
    - add local phase in logger to be added to the log's prefix (can be overwritten/updated as desired )
    
    Reviewed By: pallab-zz
    
    Differential Revision: D32298008
    
    fbshipit-source-id: a27ea0f299533c6d20d13cfe5af055c7822756e6

--- src/torch_ucc_comm.cpp
+++ src/torch_ucc_comm.cpp
@@ -12,7 +12,9 @@
 
 namespace c10d {
 
-CommUCX::CommUCX(int comm_size, const c10::intrusive_ptr<ProcessGroupUCCLogger>& logger)
+CommUCX::CommUCX(
+    int comm_size,
+    const c10::intrusive_ptr<ProcessGroupUCCLogger>& logger)
     : CommBase(logger) {
   ucp_params_t params;
   ucp_config_t* config;
@@ -43,7 +45,7 @@ CommUCX::CommUCX(int comm_size, const c10::intrusive_ptr<ProcessGroupUCCLogger>&
   worker_params.thread_mode = UCS_THREAD_MODE_MULTI;
   st = ucp_worker_create(context, &worker_params, &worker);
   if (st != UCS_OK) {
-    logger->logError(
+    TORCH_UCC_LOG_ERROR(
         TORCH_UCC_INIT,
         c10::str("UCX failed to create UCP worker:", ucs_status_string(st)));
     ucp_cleanup(context);
@@ -61,14 +63,14 @@ void CommUCX::free_request(ucc_coll_req_h request) {
 }
 
 CommUCX::~CommUCX() {
-	if (worker != nullptr) {
-  	ucp_worker_destroy(worker);
-	}
-	if (context != nullptr) {
-  	ucp_cleanup(context);
-	}
-	worker = nullptr;
-	context = nullptr;
+  if (worker != nullptr) {
+    ucp_worker_destroy(worker);
+  }
+  if (context != nullptr) {
+    ucp_cleanup(context);
+  }
+  worker = nullptr;
+  context = nullptr;
 }
 
 ucc_status_t oob_allgather(
@@ -82,16 +84,16 @@ ucc_status_t oob_allgather(
   std::vector<uint8_t> val = std::vector<uint8_t>(
       reinterpret_cast<uint8_t*>(sbuf),
       reinterpret_cast<uint8_t*>(sbuf) + msglen);
-	try {
-  	info->store->set(info->getKey("teamr" + std::to_string(info->rank)), val);
-  	info->rbuf = rbuf;
-  	info->msglen = msglen;
-  	*req = coll_info;
-	} catch (std::exception& ex) {
-		LOG(ERROR) << "(oob_allgather) Caught exception in Store Operation .. "
+  try {
+    info->store->set(info->getKey("teamr" + std::to_string(info->rank)), val);
+    info->rbuf = rbuf;
+    info->msglen = msglen;
+    *req = coll_info;
+  } catch (std::exception& ex) {
+    LOG(ERROR) << "(oob_allgather) Caught exception in Store Operation .. "
                << "[" << ex.what() << "]";
-		return UCC_ERR_NO_MESSAGE;
-	}
+    return UCC_ERR_NO_MESSAGE;
+  }
   return UCC_OK;
 }
 
@@ -99,54 +101,57 @@ ucc_status_t oob_allgather_test(void* req) {
   torch_ucc_oob_coll_info_t* info =
       reinterpret_cast<torch_ucc_oob_coll_info_t*>(req);
 
-	try {
-  	for (int r = 0; r < info->size; r++) {
-    	if (!info->store->check({info->getKey("teamr" + std::to_string(r))})) {
-      	return UCC_INPROGRESS;
-    	}
-  	}
-  	for (int r = 0; r < info->size; r++) {
-    	std::vector<uint8_t> data =
-        	info->store->get(info->getKey("teamr" + std::to_string(r)));
-    	memcpy(
-        	(void*)((ptrdiff_t)info->rbuf + info->msglen * r),
-        	data.data(),
-        	info->msglen);
-  	}
-	} catch (std::exception& ex) {
-		LOG(ERROR) << "(oob_allgather) Caught exception in Store Operation .. "
+  try {
+    for (int r = 0; r < info->size; r++) {
+      if (!info->store->check({info->getKey("teamr" + std::to_string(r))})) {
+        return UCC_INPROGRESS;
+      }
+    }
+    for (int r = 0; r < info->size; r++) {
+      std::vector<uint8_t> data =
+          info->store->get(info->getKey("teamr" + std::to_string(r)));
+      memcpy(
+          (void*)((ptrdiff_t)info->rbuf + info->msglen * r),
+          data.data(),
+          info->msglen);
+    }
+  } catch (std::exception& ex) {
+    LOG(ERROR) << "(oob_allgather) Caught exception in Store Operation .. "
                << "[" << ex.what() << "]";
-		return UCC_ERR_NO_MESSAGE;
-	}
+    return UCC_ERR_NO_MESSAGE;
+  }
   return UCC_OK;
 }
 
 ucc_status_t oob_allgather_free(void* req) {
   torch_ucc_oob_coll_info_t* info =
       reinterpret_cast<torch_ucc_oob_coll_info_t*>(req);
-	try {
-  	int num_done = info->store->add({info->getKey("ag_done")}, 1);
-  	if (num_done == info->size) {
-    	info->store->deleteKey(info->getKey("ag_done"));
-    	for (int r = 0; r < info->size; r++) {
-      	info->store->deleteKey(info->getKey("teamr" + std::to_string(r)));
-    	}
-    	for (int r = 0; r < info->size; r++) {
-      	info->store->add({info->getKey("ag_free" + std::to_string(r))}, 1);
-    	}
-  	} else {
-    	info->store->wait({info->getKey("ag_free" + std::to_string(info->rank))});
-  	}
-  	info->store->deleteKey(info->getKey("ag_free" + std::to_string(info->rank)));
-	} catch (std::exception& ex) {
-		LOG(ERROR) << "(oob_allgather) Caught exception in Store Operation .. "
+  try {
+    int num_done = info->store->add({info->getKey("ag_done")}, 1);
+    if (num_done == info->size) {
+      info->store->deleteKey(info->getKey("ag_done"));
+      for (int r = 0; r < info->size; r++) {
+        info->store->deleteKey(info->getKey("teamr" + std::to_string(r)));
+      }
+      for (int r = 0; r < info->size; r++) {
+        info->store->add({info->getKey("ag_free" + std::to_string(r))}, 1);
+      }
+    } else {
+      info->store->wait({info->getKey("ag_free" + std::to_string(info->rank))});
+    }
+    info->store->deleteKey(
+        info->getKey("ag_free" + std::to_string(info->rank)));
+  } catch (std::exception& ex) {
+    LOG(ERROR) << "(oob_allgather) Caught exception in Store Operation .. "
                << "[" << ex.what() << "]";
-		return UCC_ERR_NO_MESSAGE;
-	}
+    return UCC_ERR_NO_MESSAGE;
+  }
   return UCC_OK;
 }
 
-CommUCC::CommUCC(torch_ucc_oob_coll_info_t* oob_info, const c10::intrusive_ptr<ProcessGroupUCCLogger>& logger)
+CommUCC::CommUCC(
+    torch_ucc_oob_coll_info_t* oob_info,
+    const c10::intrusive_ptr<ProcessGroupUCCLogger>& logger)
     : CommBase(logger) {
   ucc_lib_config_h lib_config;
   ucc_context_config_h context_config;
@@ -167,12 +172,18 @@ CommUCC::CommUCC(torch_ucc_oob_coll_info_t* oob_info, const c10::intrusive_ptr<P
   lib_attr.mask = UCC_LIB_ATTR_FIELD_THREAD_MODE;
   TORCH_UCC_CHECK(
       ucc_lib_get_attr(lib, &lib_attr), "failed to query for lib attr");
-  TORCH_CHECK(lib_attr.thread_mode == UCC_THREAD_MULTIPLE, "ucc library wasn't initialized with mt support, please check ucc compile options ");
+  TORCH_CHECK(
+      lib_attr.thread_mode == UCC_THREAD_MULTIPLE,
+      "ucc library wasn't initialized with mt support, please check ucc compile options ");
   st = ucc_context_config_read(lib, NULL, &context_config);
   if (st != UCC_OK) {
     // FIXME: would this cause deadlock if only one rank fails?
-    TORCH_UCC_CHECK(ucc_finalize(lib), "failed to finalize UCC library when failing to read UCC context config");
-    logger->logError(TORCH_UCC_INIT, c10::str("failed to read UCC context config: ", ucc_status_string(st)));
+    TORCH_UCC_CHECK(
+        ucc_finalize(lib),
+        "failed to finalize UCC library when failing to read UCC context config");
+    TORCH_UCC_LOG_ERROR(
+        TORCH_UCC_INIT,
+        c10::str("failed to read UCC context config: ", ucc_status_string(st)));
     throw std::runtime_error(ucc_status_string(st));
   }
   st = ucc_context_config_modify(
@@ -183,7 +194,11 @@ CommUCC::CommUCC(torch_ucc_oob_coll_info_t* oob_info, const c10::intrusive_ptr<P
   if (st != UCC_OK) {
     ucc_context_config_release(context_config);
     ucc_finalize(lib);
-    logger->logError(TORCH_UCC_INIT, c10::str("UCC failed to modify UCC context config: ", ucc_status_string(st)));
+    TORCH_UCC_LOG_ERROR(
+        TORCH_UCC_INIT,
+        c10::str(
+            "UCC failed to modify UCC context config: ",
+            ucc_status_string(st)));
     throw std::runtime_error(ucc_status_string(st));
   }
   memset(&context_params, 0, sizeof(ucc_context_params_t));
@@ -199,33 +214,44 @@ CommUCC::CommUCC(torch_ucc_oob_coll_info_t* oob_info, const c10::intrusive_ptr<P
   st = ucc_context_create(lib, &context_params, context_config, &context);
   ucc_context_config_release(context_config);
   if (st != UCC_OK) {
-    TORCH_UCC_CHECK(ucc_finalize(lib), "failed to finalize UCC library when failing to creat UCC context");
-    logger->logError(TORCH_UCC_INIT, c10::str("UCC failed to create UCC context: ", ucc_status_string(st)));
+    TORCH_UCC_CHECK(
+        ucc_finalize(lib),
+        "failed to finalize UCC library when failing to creat UCC context");
+    TORCH_UCC_LOG_ERROR(
+        TORCH_UCC_INIT,
+        c10::str("UCC failed to create UCC context: ", ucc_status_string(st)));
     throw std::runtime_error(ucc_status_string(st));
   }
 }
 
 void CommUCC::progress() {
-  TORCH_UCC_CHECK(ucc_context_progress(context), "failed to progress UCC collective");
+  TORCH_UCC_CHECK(
+      ucc_context_progress(context), "failed to progress UCC collective");
 }
 
 void CommUCC::free_request(ucc_coll_req_h request) {
-  TORCH_UCC_CHECK(ucc_collective_finalize(request), "failed to release UCC request");
+  TORCH_UCC_CHECK(
+      ucc_collective_finalize(request), "failed to release UCC request");
 }
 
 CommUCC::~CommUCC() {
-	if (context != nullptr) {
-  	TORCH_UCC_CHECK(ucc_context_destroy(context), "failed to destory UCC context");
-	}
-	if (lib != nullptr) {
-  	TORCH_UCC_CHECK(ucc_finalize(lib), "failed to finalize UCC library");
-	}
-	context = nullptr;
-	lib = nullptr;
+  if (context != nullptr) {
+    TORCH_UCC_CHECK(
+        ucc_context_destroy(context), "failed to destory UCC context");
+  }
+  if (lib != nullptr) {
+    TORCH_UCC_CHECK(ucc_finalize(lib), "failed to finalize UCC library");
+  }
+  context = nullptr;
+  lib = nullptr;
 }
 
-std::string ProcessGroupUCCLogger::getLogPrefix() {
-  return log_prefix;
+std::string ProcessGroupUCCLogger::getLogPrefix(torch_ucc_phase_t phase) {
+  // caller can override the phase stored locally
+  torch_ucc_phase_t phase_ =
+      (local_phase != phase && phase != TORCH_UCC_UNKNOWN) ? phase
+                                                           : local_phase;
+  return c10::str(log_prefix, "[", ucc_phase_map.at(phase_), "]");
 }
 void ProcessGroupUCCLogger::setLogPrefix(std::string log_prefix_) {
   log_prefix = log_prefix_;
@@ -234,10 +260,11 @@ void ProcessGroupUCCLogger::setLogPrefix(std::string log_prefix_) {
 ProcessGroupUCCLogger::ProcessGroupUCCLogger() {
   setLogPrefix("[ProcessGroupUCC]");
 }
-ProcessGroupUCCLogger::ProcessGroupUCCLogger(std::string log_prefix) {
+ProcessGroupUCCLogger::ProcessGroupUCCLogger(
+    std::string log_prefix,
+    torch_ucc_phase_t phase)
+    : local_phase(phase) {
   setLogPrefix(log_prefix);
 }
-
-
 
 } // namespace c10d
