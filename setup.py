d626094b93ef1447714048fefff9967a17298539
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-12-09 13:43:46 -0800

    save

--- setup.py
+++ setup.py
@@ -40,20 +40,20 @@ plugin_library_dirs = ["{}/lib/".format(ucx_home),
                        "{}/lib/".format(ucc_home)]
 plugin_libraries    = ["ucp", "uct", "ucm", "ucs", "ucc"]
 
+if '--oss' in sys.argv:
+  sys.argv.remove('--oss')
+  plugin_sources += ["src/torch_ucc_init_oss.cpp"]
+  name = "torch_ucc_oss"
+else:
+  plugin_sources += ["src/torch_ucc_init.cpp"]
+  name = "torch_ucc"
+
 with_cuda = os.environ.get("WITH_CUDA")
 if with_cuda is None or with_cuda == "no":
     print("CUDA support is disabled")
     module = cpp_extension.CppExtension(
-        name = "torch_ucc",
-        sources = plugin_sources + ["src/torch_ucc_init.cpp"],
-        include_dirs = plugin_include_dirs,
-        library_dirs = plugin_library_dirs,
-        libraries = plugin_libraries,
-        extra_compile_args=plugin_compile_args
-    )
-    module_oss = cpp_extension.CppExtension(
-        name = "torch_ucc_oss",
-        sources = plugin_sources + ["src/torch_ucc_init_oss.cpp"],
+        name = name,
+        sources = plugin_sources,
         include_dirs = plugin_include_dirs,
         library_dirs = plugin_library_dirs,
         libraries = plugin_libraries,
@@ -63,24 +63,16 @@ else:
     print("CUDA support is enabled")
     plugin_compile_args.append("-DUSE_CUDA")
     module = cpp_extension.CUDAExtension(
-        name = "torch_ucc",
-        sources = plugin_sources + ["src/torch_ucc_init.cpp"],
-        include_dirs = plugin_include_dirs,
-        library_dirs = plugin_library_dirs,
-        libraries = plugin_libraries,
-        extra_compile_args=plugin_compile_args
-    )
-    module_oss = cpp_extension.CUDAExtension(
-        name = "torch_ucc_oss",
-        sources = plugin_sources + ["src/torch_ucc_init_oss.cpp"],
+        name = name,
+        sources = plugin_sources,
         include_dirs = plugin_include_dirs,
         library_dirs = plugin_library_dirs,
         libraries = plugin_libraries,
         extra_compile_args=plugin_compile_args
     )
 setup(
-    name = "torch-ucc",
+    name = name,
     version = "1.0.0",
-    ext_modules = [module, module_oss],
+    ext_modules = [module],
     cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
