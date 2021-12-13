2f35898639c839eea03e87ca19faf66c6da28694
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-12-13 10:54:32 -0800

    save

--- src/torch_ucc.cpp
+++ src/torch_ucc.cpp
@@ -1271,18 +1271,3 @@ void ProcessGroupUCC::initComm(c10::Device dev) {
 }
 
 } // namespace c10d
-
-// This function is intentionally designed to take a void * argument
-// and return a void * argument. This design is to make the mangled
-// symbol simple.
-C10_EXPORT void *createProcessGroupUCCForNCCL(void *args) {
-  using namespace c10d;
-  struct args_t {
-    const c10::intrusive_ptr<Store>& store;
-    int rank = -1;
-    int size = -1;
-  };
-  args_t *args_ = static_cast<args_t *>(args);
-  c10d::ProcessGroupUCC *pg = new ProcessGroupUCC(args_->store, args_->rank, args_->size);
-  return static_cast<void *>(pg);
-}
