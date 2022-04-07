fc0b7b2ad40b9c4602bc197d0e6c9564f9af97e8
Gao, Xiang <qasdfgtyuiop@gmail.com>
2022-04-07 15:09:42 -0700

    Add a function to create ProcessGroupUCC in PyTorch (#57)
    Summary:
    This PR will be used in https://github.com/pytorch/pytorch/pull/69564. This PR can be landed separately and doesn't need to wait for https://github.com/pytorch/pytorch/pull/69564.
    
    Pull Request resolved: https://github.com/facebookresearch/torch_ucc/pull/57
    
    Reviewed By: pallab-zz
    
    Differential Revision: D35288300
    
    Pulled By: kingchc
    
    fbshipit-source-id: 0e08f94eca1a83bfe18ad7761cf4ab133905e1f7

--- src/torch_ucc_init_oss.cpp
+++ src/torch_ucc_init_oss.cpp
@@ -1,4 +1,13 @@
 #include <torch/python.h>
+#include "torch_ucc.hpp"
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+using namespace c10d;
+
+extern "C" C10_EXPORT c10::intrusive_ptr<ProcessGroup> createProcessGroupUCC(
+  const c10::intrusive_ptr<Store>& store,
+  int rank, int size
+) {
+  return c10::make_intrusive<ProcessGroupUCC>(store, rank, size);
 }
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
